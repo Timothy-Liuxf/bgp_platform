28dac31ec8462ed90f2141125f9cb44ce3689310
Timothy Liu <liuxf19@163.com>
2022-12-21 00:25:18 +0800

    build: :construction_worker: add autotools build system

--- src/bgp_platform/detector/init_info.cc
+++ src/bgp_platform/detector/init_info.cc
@@ -19,7 +19,7 @@ InitInfo::InitInfo(fs::path as_info_path, fs::path top_nx_path,
     std::ifstream as_info_file(as_info_path);
     auto          as_info_json = jsoncons::json::parse(as_info_file);
     for (const auto& as_info : as_info_json.object_range()) {
-      auto as_num       = AsNum {std::stoi(as_info.key())};
+      auto as_num       = static_cast<AsNum>(std::stoi(as_info.key()));
       auto as_info_data = as_info.value().as<AsInfo>();
       this->as_info_.emplace(as_num, as_info_data);
     }
@@ -33,6 +33,7 @@ InitInfo::InitInfo(fs::path as_info_path, fs::path top_nx_path,
   }
   {
     // TODO: Read top nx
+    (void)top_nx_path;
   }
   return;
 }
