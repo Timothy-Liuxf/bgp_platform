7822f5f8a2e8190ff7c033526c3e607be8842ada
Timothy Liu <liuxf19@163.com>
2022-12-24 23:08:42 +0800

    style(sql): :art: format sql with sql formatter

--- src/bgp_platform/database/sql/insert_prefix_outage_event.sql
+++ src/bgp_platform/database/sql/insert_prefix_outage_event.sql
@@ -1,21 +1,32 @@
-INSERT INTO {table_name} (
-    asn,
-    country,
-    as_name,
-    org_name,
-    as_type,
-    s_time,
-    e_time,
-    duration,
-    pre_vp_paths,
-    eve_vp_paths,
-    outage_level,
-    outage_level_descr,
-    prefix,
-    outage_id
-)
+INSERT INTO `{table_name}` (
+        asn,
+        country,
+        as_name,
+        org_name,
+        as_type,
+        s_time,
+        e_time,
+        duration,
+        pre_vp_paths,
+        eve_vp_paths,
+        outage_level,
+        outage_level_descr,
+        prefix,
+        outage_id
+    )
 VALUES (
-    '{asn}', '{country}', '{as_name}', '{org_name}', '{as_type}', '{s_time:%Y-%m-%d %H:%M:%S}', '{e_time:%Y-%m-%d %H:%M:%S}',
-    '{duration}', '{pre_vp_paths}', '{eve_vp_paths}', '{outage_level}', '{outage_level_descr}', 
-    '{prefix}', '{outage_id}'
-);
+        '{asn}',
+        '{country}',
+        '{as_name}',
+        '{org_name}',
+        '{as_type}',
+        '{s_time:%Y-%m-%d %H:%M:%S}',
+        '{e_time:%Y-%m-%d %H:%M:%S}',
+        '{duration}',
+        '{pre_vp_paths}',
+        '{eve_vp_paths}',
+        '{outage_level}',
+        '{outage_level_descr}',
+        '{prefix}',
+        '{outage_id}'
+    );
