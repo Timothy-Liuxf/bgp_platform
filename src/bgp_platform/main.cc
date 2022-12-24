16612e9dd458c6497405072fabdca3d45b4164f2
Timothy Liu <liuxf19@163.com>
2022-12-25 04:04:03 +0800

    feat(config): :sparkles: read config.json to get params

--- src/bgp_platform/main.cc
+++ src/bgp_platform/main.cc
@@ -1,11 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <jsoncons/json.hpp>
+
 #include <bgp_platform/detector/detector.hpp>
 
 using namespace bgp_platform;
+using namespace jsoncons;
+using namespace std::literals::string_literals;
 
 int main() {
-  Detector detector(
-      {"info/new_as_dict.json", "info/top_nx.csv", "info/top_ip.txt"},
-      {"localhost", "5432", "testuser", "testpasswd", "testdb"});
+  const auto    config_path = "config/config.json"s;
+  std::ifstream config_file(config_path, std::ios::in);
+  if (!config_file) {
+    std::cerr << "Cannot open config file: " << config_path << std::endl;
+    return 1;
+  }
+
+  auto config = json::parse(config_file);
+  config_file.close();
+
+  const json& data_path = config["data-path"];
+  const json& db_config = config["database"];
+
+  Detector    detector(
+         {data_path["as-dict"].as_string_view(),
+          data_path["top-nx"].as_string_view(),
+          data_path["top-ip"].as_string_view()},
+         {db_config["host"].as_string_view(), db_config["port"].as_string_view(),
+          db_config["user"].as_string_view(),
+          db_config["password"].as_string_view(),
+          db_config["database"].as_string_view()});
   detector.Detect("build");
   return 0;
 }
